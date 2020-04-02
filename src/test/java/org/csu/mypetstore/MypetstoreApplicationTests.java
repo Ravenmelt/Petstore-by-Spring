34bf42c978eb9841a622fd05cd653c9bb21721c4
86139 <1846344092@qq.com>
2020-04-02 19:09:04 +0800

    mybatis

--- src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -1,13 +1,27 @@
 package org.csu.mypetstore;
 
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.service.CatalogService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@MapperScan("org.csu.mypetstore.persistence")
 class MypetstoreApplicationTests {
+
+    @Autowired
+    CatalogService catalogService;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testCategory(){
+        Category c =catalogService.getCategory("BIRDS");
+        System.out.println(c.getCategoryId()+","+c.getName()+","+c.getDescription());
     }
 
 }
