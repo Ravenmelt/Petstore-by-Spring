231430d3e1f1e3b0390b55df81517e4709cf46a1
86139 <1846344092@qq.com>
2020-04-03 08:58:16 +0800

    Mypetstore

--- src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -1,27 +1,76 @@
 package org.csu.mypetstore;
 
-import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.*;
+import org.csu.mypetstore.service.AccountService;
+//import org.csu.mypetstore.service.CartService;
 import org.csu.mypetstore.service.CatalogService;
+import org.csu.mypetstore.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+
 @SpringBootTest
 @MapperScan("org.csu.mypetstore.persistence")
 class MypetstoreApplicationTests {
-
     @Autowired
     CatalogService catalogService;
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    OrderService orderService;
+//    @Autowired
+//    CartService cartService;
 
     @Test
     void contextLoads() {
     }
 
     @Test
-    void testCategory(){
-        Category c =catalogService.getCategory("BIRDS");
-        System.out.println(c.getCategoryId()+","+c.getName()+","+c.getDescription());
+    void testCatagory(){
+        Category c=catalogService.getCategory("BIRDS");
+        System.out.println(c.getName()+","+ c.getDescription());
     }
+    @Test
+    void testProdect(){
+        List<Product> productList=catalogService.getProductListByCategory("BIRDS");
+        System.out.print(productList.size());
+    }
+    @Test
+    void testItem(){
+        Item item=catalogService.getItem("EST-14");
+        System.out.println(item.getItemId()+" ,"+item.getListPrice()+","+item.getAttribute1());
+    }
+    @Test
+    void testAccount(){
+        Account account=accountService.getAccount("j2ee");
+        System.out.println(account.getAddress1()+","+account.getEmail()+"1");
+
+        Account account1=accountService.getAccount("j2ee","j2ee");
+        System.out.println(account1.getEmail()+","+account.getUsername()+"2");
+//
+
+    }
+
+
+
+//    @Test
+//    void testCart(){
+//      CartItem cartItem=cartService.getCartByUsername("j2ee");
+//      System.out.println(cartItem.getItem());
+////        Account account=accountService.getAccount("j2ee");
+////        CartItem cartItem=new CartItem();
+////        Item item=catalogService.getItem("EST-14");
+////        cartItem.setItem(item);
+////        cartItem.setQuantity(5);
+////        cartItem.setInStock(false);
+////        cartService.insertCartItem(cartItem,account);
+//
+//
+//    }
 
 }
